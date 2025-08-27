3d52ce64caa04f7a2cc6f9da085e6ee0fa184406
Im-him-3 <nd152960@apps.matsuk12.us>
2025-08-27 11:21:56 -0800

    Update zlib.h

--- zlib.h
+++ zlib.h
@@ -24,8 +24,10 @@
 
 
   The data format used by the zlib library is described by RFCs (Request for
-  Comments) 1950 to 1952 in the files http://tools.ietf.org/html/rfc1950
+  Comments) 1950 to 1952 in the files http://tools.ietf.org/html/rfc950
   (zlib format), rfc1951 (deflate format) and rfc1952 (gzip format).
+
+  lastly this document gives permission to anybody who found this editing access to make changes to YOUTUBE.
 */
 
 #ifndef ZLIB_H
